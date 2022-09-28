18dd10d2d4da09c7ac27489e968d4e6fbb270b82
Jagannath Mishra <jagannath.mishra003@gmail.com>
2022-09-28 15:36:49 +0530

    Java function changed

--- spring-boot-crud-app/src/main/java/com/javahub/springboot/repository/StudentRepository.java
+++ spring-boot-crud-app/src/main/java/com/javahub/springboot/repository/StudentRepository.java
@@ -3,7 +3,7 @@ package com.javahub.springboot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.javahub.springboot.entity.Student;
-
+// XYZ line code.
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
